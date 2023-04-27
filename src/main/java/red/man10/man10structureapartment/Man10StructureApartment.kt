eef1c5ae6ba7800ba1c14048c6a1a9abd2854107
forest611 <forest611328@gmail.com>
2023-04-27 19:23:02 +0900

    Update

--- src/main/java/red/man10/man10structureapartment/Man10StructureApartment.kt
+++ src/main/java/red/man10/man10structureapartment/Man10StructureApartment.kt
@@ -1,5 +1,8 @@
 package red.man10.man10structureapartment
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class Man10StructureApartment : JavaPlugin() {
@@ -11,9 +14,39 @@ class Man10StructureApartment : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         instance = this
+        getCommand("msa")!!.setExecutor(this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+
+        if (sender !is Player)return true
+
+        if (args.isNullOrEmpty()){
+            return true
+        }
+
+        when(args[0]){
+
+            "save"->{
+
+            }
+
+            "load"->{
+
+            }
+
+
+        }
+
+        return true
+    }
+
+    fun loadApartment(p:Player){
+
+
     }
 }
--- src/main/java/red/man10/man10structureapartment/StructureManager.kt
+++ src/main/java/red/man10/man10structureapartment/StructureManager.kt
@@ -2,22 +2,35 @@ package red.man10.man10structureapartment
 
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.block.structure.Mirror
 import org.bukkit.block.structure.StructureRotation
 import red.man10.man10structureapartment.Man10StructureApartment.Companion.instance
 import java.io.File
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
 
 object StructureManager {
 
-    fun save(pos1:Location,pos2:Location,apartName:String,owner:UUID){
+
+    fun getApartInfo(apartName: String){
+
+    }
+
+    //  ストラクチャーの保存
+    //  ownerがnullの場合は、初期建築を保存する
+    fun save(pos1:Location,pos2:Location,apartName:String,owner:UUID?){
 
         val manager = instance.server.structureManager
         val structure = manager.createStructure()
 
         structure.fill(pos1,pos2,true)
 
-        val file = File("${instance.dataFolder.path}/${apartName}/${owner}")
+        val fileName = owner?.toString() ?: "Default"
+
+        val file = File("${instance.dataFolder.path}/${apartName}/${fileName}")
 
         try {
             if (!file.exists()){
@@ -31,13 +44,52 @@ object StructureManager {
         }
     }
 
+    //  ストラクチャーの呼び出し
     fun place(owner: UUID,apartName: String,pos:Location){
         val manager = instance.server.structureManager
         val file = File("${instance.dataFolder.path}/${apartName}/${owner}")
-        val structure = manager.loadStructure(file)
 
-        structure.place(pos,true,StructureRotation.NONE,Mirror.NONE,0,1F, Random())
-        
+        val structure = if (file.exists()){
+            manager.loadStructure(file)
+        } else {
+            val default = File("${instance.dataFolder.path}/${apartName}/Default")
+            if (!default.exists()){
+                Bukkit.getLogger().warning("初期建築がありません")
+                return
+            }
+            manager.loadStructure(default)
+        }
+
+        //TODO:要検証
+        structure.place(pos,true,StructureRotation.NONE,Mirror.NONE,-1,1F, Random())
+    }
+
+    fun remove(pos1:Location,pos2:Location){
+        val world = pos1.world
+
+        val minX = min(pos1.blockX,pos2.blockX)
+        val minY = min(pos1.blockY,pos2.blockY)
+        val minZ = min(pos1.blockZ,pos2.blockZ)
+        val maxX = max(pos1.blockX,pos2.blockX)
+        val maxY = max(pos1.blockY,pos2.blockY)
+        val maxZ = max(pos1.blockZ,pos2.blockZ)
+
+        //エンティティを削除
+        for (e in world.entities){
+            val loc = e.location
+            if (loc.blockX in minX..maxX && loc.blockY in minY..maxY && loc.blockZ in minZ .. maxZ){
+                e.remove()
+            }
+        }
+
+        //ブロックを削除
+        for (x in minX..maxX) {
+            for (y in minY..maxY) {
+                for (z in minZ..maxZ) {
+                    world.getBlockAt(x, y, z).type = Material.AIR
+                }
+            }
+        }
     }
 
 
