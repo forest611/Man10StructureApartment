9e63a59c8dc88df75861fb350f7b996bd2ab770d
Jin Morikawa <forest611328@gmail.com>
2023-05-03 15:21:59 +0900

    建物の設置、保存処理までできた

--- src/main/java/red/man10/man10structureapartment/Man10StructureApartment.kt
+++ src/main/java/red/man10/man10structureapartment/Man10StructureApartment.kt
@@ -1,29 +1,47 @@
 package red.man10.man10structureapartment
 
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.NamespacedKey
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 import red.man10.man10structureapartment.StructureManager.distance
 import red.man10.man10structureapartment.StructureManager.maxApartCount
 import red.man10.man10structureapartment.StructureManager.world
 
-class Man10StructureApartment : JavaPlugin() {
+class Man10StructureApartment : JavaPlugin(),Listener {
 
     companion object{
         lateinit var instance : Man10StructureApartment
+
+        fun locToStr(loc:Location):String{
+            return "${loc.world.name};${loc.x};${loc.y};${loc.z}"
+        }
+        fun strToLoc(str:String):Location{
+            val array = str.split(";")
+            return Location(Bukkit.getWorld(array[0]),array[1].toDouble(),array[2].toDouble(),array[3].toDouble())
+        }
     }
 
     override fun onEnable() {
         // Plugin startup logic
         saveDefaultConfig()
+        instance = this
 
         distance = config.getInt("Distance")
         maxApartCount = config.getInt("MaxApartCount")
-        world = instance.server.getWorld(config.getString("BuilderWorld")?:"builder")!!
+        world = server.getWorld(config.getString("BuilderWorld")?:"world")!!
 
-        instance = this
         getCommand("msa")!!.setExecutor(this)
+        server.pluginManager.registerEvents(this,this)
+
+        StructureManager.load()
     }
 
     override fun onDisable() {
@@ -41,17 +59,71 @@ class Man10StructureApartment : JavaPlugin() {
         when(args[0]){
 
             "save"->{
-
+                StructureManager.saveStructure(sender)
             }
 
-            "load"->{
+            "place"->{
+                StructureManager.placeStructure(sender)
+            }
+
+            "remove" ->{
+                StructureManager.remove(sender.uniqueId)
             }
 
             "jump" ->{
+            }
+
+            "default" ->{
+                sender.persistentDataContainer.set(NamespacedKey(this,"SetDefault"), PersistentDataType.INTEGER,1)
+                sender.sendMessage("始点と終点を殴って範囲を指定する")
             }
         }
 
         return true
     }
+
+    @EventHandler
+    fun breakEvent(e:BlockBreakEvent){
+        val p = e.player
+
+        val mode = p.persistentDataContainer[NamespacedKey(this,"SetDefault"), PersistentDataType.INTEGER] ?: return
+
+        //POS1
+        if (mode == 1){
+            p.persistentDataContainer
+                .set(NamespacedKey(this,"Pos1"), PersistentDataType.STRING,locToStr(e.block.location))
+            p.persistentDataContainer.set(NamespacedKey(this,"SetDefault"), PersistentDataType.INTEGER,2)
+            p.sendMessage("始点を設定 終点を殴る")
+            return
+        }
+
+        //POS2
+        if (mode == 2){
+            p.persistentDataContainer
+                .set(NamespacedKey(this,"Pos2"), PersistentDataType.STRING,locToStr(e.block.location))
+            p.persistentDataContainer.set(NamespacedKey(this,"SetDefault"), PersistentDataType.INTEGER,3)
+            p.sendMessage("終点を設定 スポーン地点を殴る(殴った時のあなたの位置がスポーン地点になる)")
+            return
+        }
+
+        //Spawn
+        if (mode == 3){
+            val pos1Str = p.persistentDataContainer[NamespacedKey(this,"Pos1"), PersistentDataType.STRING]?:return
+            val pos2Str = p.persistentDataContainer[NamespacedKey(this,"Pos2"), PersistentDataType.STRING]?:return
+            val pos1 = strToLoc(pos1Str)
+            val pos2 = strToLoc(pos2Str)
+            val spawn = p.location
+
+            p.persistentDataContainer.remove(NamespacedKey(this,"Pos1"))
+            p.persistentDataContainer.remove(NamespacedKey(this,"Pos2"))
+            p.persistentDataContainer.remove(NamespacedKey(this,"SetDefault"))
+
+            p.sendMessage("スポーンを設定")
+
+            StructureManager.saveDefault(p,pos1, pos2,spawn)
+        }
+
+    }
+
 
 }
--- src/main/java/red/man10/man10structureapartment/StructureManager.kt
+++ src/main/java/red/man10/man10structureapartment/StructureManager.kt
@@ -1,19 +1,19 @@
 package red.man10.man10structureapartment
 
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import org.bukkit.Bukkit
-import org.bukkit.Location
-import org.bukkit.Material
-import org.bukkit.World
+import org.bukkit.*
 import org.bukkit.block.structure.Mirror
 import org.bukkit.block.structure.StructureRotation
+import org.bukkit.entity.Player
+import org.bukkit.structure.Structure
+import org.bukkit.structure.StructureManager
 import red.man10.man10structureapartment.Man10StructureApartment.Companion.instance
+import red.man10.man10structureapartment.Man10StructureApartment.Companion.locToStr
+import red.man10.man10structureapartment.Man10StructureApartment.Companion.strToLoc
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 import java.util.*
-import kotlin.collections.HashMap
 import kotlin.math.max
 import kotlin.math.min
 
@@ -24,33 +24,68 @@ object StructureManager {
     var maxApartCount = 128
     lateinit var world: World
 
-    private var ownerData = HashMap<UUID,ApartData>()
+    private var addressMap = HashMap<UUID,ApartData>()
+    private lateinit var manager : StructureManager
+    private lateinit var defaultBuilding : Structure
 
-    //  開いているマンションの数などを呼び出す
-    private fun getApartCount():Int{
-        return ownerData.size
+    fun load(){
+        manager = instance.server.structureManager
+        loadDefault()
+        loadAddress()
     }
 
-    private fun loadOwnerData(){
+    //初期建築の読み込み
+    private fun loadDefault(){
+        val default = File("${instance.dataFolder.path}/Apart/Default")
+        if (!default.exists()){
+            Bukkit.getLogger().warning("初期建築がありません")
+            return
+        }
+        defaultBuilding = manager.loadStructure(default)
+    }
 
-        ownerData.clear()
+    fun saveDefault(p:Player, pos1:Location, pos2:Location,spawn:Location){
+        val structure = manager.createStructure()
+        structure.fill(pos1,pos2,true)
 
-        val file = File("${instance.dataFolder.path}/OwnerData.json")
+        val file = File("${instance.dataFolder.path}/Apart/Default")
 
-        if (!file.exists())return
+        try {
+            manager.saveStructure(file,structure)
+            defaultBuilding = structure
+            p.sendMessage("保存成功")
+        }catch (e:Exception){
+            p.sendMessage("§c初期建築の保存に失敗")
+        }
+    }
+
+    private fun loadAddress(){
+
+        addressMap.clear()
+
+        val file = File("${instance.dataFolder.path}/Address.json")
+
+        if (!file.exists()){
+            Bukkit.getLogger().warning("住所情報がありません")
+            return
+        }
 
         val reader = FileReader(file)
 
-        ownerData = Gson().fromJson(reader.readText(),object : TypeToken<HashMap<UUID,ApartData>>(){}.type)
+        val array = Gson().fromJson(reader.readText(),Array<ApartData>::class.java)
+
+        array.forEach {
+            addressMap[it.owner] = it
+        }
 
         reader.close()
     }
 
-    private fun saveOwnerData(){
+    private fun saveAddress(){
 
-        val jsonStr = Gson().toJson(ownerData)
+        val jsonStr = Gson().toJson(addressMap.values.toTypedArray())
 
-        val file = File("${instance.dataFolder.path}/OwnerData.json")
+        val file = File("${instance.dataFolder.path}/Address.json")
 
         val writer = FileWriter(file)
 
@@ -59,27 +94,24 @@ object StructureManager {
         writer.close()
     }
 
-    private fun set(data: ApartData){
-        ownerData[data.owner] = data
-        saveOwnerData()
+    private fun update(data: ApartData){
+        addressMap[data.owner] = data
+        saveAddress()
     }
 
     //  ストラクチャーの保存
-    fun save(owner:UUID){
+    fun saveStructure(p:Player){
 
-        val manager = instance.server.structureManager
         val structure = manager.createStructure()
 
-        val data = ownerData[owner]?:return
+        val data = addressMap[p.uniqueId]?:return
 
-        val pos1 = data.pos1
-        val pos2 = data.pos2
+        val pos1 = strToLoc(data.pos1)
+        val pos2 = strToLoc(data.pos2)
 
         structure.fill(pos1,pos2,true)
 
-        val fileName = owner.toString()
-
-        val file = File("${instance.dataFolder.path}/Apart/${fileName}")
+        val file = File("${instance.dataFolder.path}/Apart/${p.uniqueId}")
 
         try {
             if (!file.exists()){
@@ -88,41 +120,44 @@ object StructureManager {
                 Bukkit.getLogger().info("上書きします")
                 manager.saveStructure(file,structure)
             }
+            p.sendMessage("保存完了")
         }catch (e:Exception){
-            Bukkit.getLogger().warning("書き込みエラー 持ち主:${owner}")
+            p.sendMessage("§cアパートの保存に失敗しました。レポートしてください")
         }
+
     }
 
     //  ストラクチャーの呼び出し
-    fun place(owner: UUID){
+    fun placeStructure(p:Player){
 
-        if (ownerData[owner]!=null)return
-
-        //座標を仮設定
-        var pos1 = Location(world,(getApartCount() * distance).toDouble(),60.0,0.0)
-
-        //最大数に達していたら、一番古いアパートを削除する
-        if (getApartCount()>= maxApartCount){
-            val oldestData = ownerData.values.minByOrNull { it.lastAccess }!!
-            remove(oldestData.owner)
-            pos1 = oldestData.pos1
+        if (addressMap[p.uniqueId]!=null){
+            p.sendMessage("すでに建物があります")
+            return
         }
 
-        val manager = instance.server.structureManager
-        val file = File("${instance.dataFolder.path}/Apart/${owner}")
+        //座標を仮設定(現在あるアパートの数から指定する
+        var pos1 = Location(world,(addressMap.size * distance).toDouble(),60.0,0.0)
+
+        //最大数に達していたら、一番古いアパートを削除する
+        if (addressMap.size >= maxApartCount){
+            val oldestData = addressMap.values.minByOrNull { it.lastAccess }!!
+            remove(oldestData.owner)
+            pos1 = strToLoc(oldestData.pos1)
+        }
+
+        val file = File("${instance.dataFolder.path}/Apart/${p.uniqueId}")
 
         val structure = if (file.exists()){
             manager.loadStructure(file)
         } else {
             val default = File("${instance.dataFolder.path}/Apart/Default")
             if (!default.exists()){
-                Bukkit.getLogger().warning("初期建築がありません")
+                p.sendMessage("§cアパートの初期値がありません。レポートしてください")
                 return
             }
             manager.loadStructure(default)
         }
 
-        //TODO:要検証
         structure.place(pos1,true,StructureRotation.NONE,Mirror.NONE,-1,1F, Random())
 
         val pos2 = pos1.clone()
@@ -131,15 +166,19 @@ object StructureManager {
         pos2.y+=structure.size.y
         pos2.z+=structure.size.z
 
-        set(ApartData(owner,pos1,pos2, Date()))
+        //住所情報をJsonファイルに登録
+        update(ApartData(p.uniqueId, locToStr(pos1), locToStr(pos2), Date()))
+
+        p.sendMessage("設置完了")
     }
 
+    //土地を削除する
     fun remove(owner: UUID){
 
-        val data = ownerData[owner]?:return
+        val data = addressMap[owner]?:return
 
-        val pos1 = data.pos1
-        val pos2 = data.pos2
+        val pos1 = strToLoc(data.pos1)
+        val pos2 = strToLoc(data.pos2)
 
         val world = pos1.world
 
@@ -167,14 +206,27 @@ object StructureManager {
             }
         }
 
-        ownerData.remove(owner)
-        saveOwnerData()
+        addressMap.remove(owner)
+        saveAddress()
+    }
+
+    fun jump(p:Player){
+
+        val data = addressMap[p.uniqueId]
+
+        if (data == null){
+            p.sendMessage("あなたはマンションを借りていません")
+            return
+        }
+
+
+
     }
 }
 
 data class ApartData(
     val owner : UUID,
-    val pos1 : Location,
-    val pos2 : Location,
+    val pos1 : String,
+    val pos2 : String,
     val lastAccess : Date
 )
